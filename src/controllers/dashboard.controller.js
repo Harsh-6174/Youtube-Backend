063eceabaad0831952d802b8d84fa46db37845c5
Harsh <harshprsh.14@gmail.com>
2024-03-23 11:11:08 +0530

    Completed Dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,79 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user?._id
+
+    try {
+        const channelStats = await Video.aggregate(
+            {
+                $match: {
+                    owner: new mongoose.Types.ObjectId(userId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "likes",
+                    localField: "_id",
+                    foreignField: "video",
+                    as: "Likes"
+                }
+            },
+            {
+                $lookup: {
+                    from: "subscriptions",
+                    localField: "owner",
+                    foreignField: "channel",
+                    as: "Subcribers"
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    TotalVideos: {
+                        $sum: 1
+                    },
+                    TotalViews: {
+                        $sum: "$views"
+                    },
+                    TotalSubscribers: {
+                        $first: {
+                            $size: "$Subcribers"
+                        }
+                    },
+                    TotalLikes: {
+                        $first: {
+                            $size: "$Likes"
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    TotalSubscribers: 1,
+                    TotalLikes: 1,
+                    TotalViews: 1,
+                    TotalVideos: 1
+                }
+            }
+        )
+
+        if(!channelStats)
+        {
+            throw new ApiError(404, "Channel stats not found")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                channelStats[0],
+                "Channel stats fetched successfully"
+            )
+        )
+    } 
+    catch (error) {
+        throw new ApiError(500, error?.message || "Error while fetching channel stats")
+    }
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
